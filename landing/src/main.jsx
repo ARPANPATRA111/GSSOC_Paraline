5ea82ad24869d0ab132cfef044508076b7b2d50a
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-05-08 23:13:39 +0530

    Update main.jsx

--- landing/src/main.jsx
+++ landing/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+window.dataLayer = window.dataLayer || [];
+window.gtag = window.gtag || function gtagProxy() {
+  window.dataLayer.push(arguments);
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
